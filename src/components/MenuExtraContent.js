8bb867043827ab83715f6db170ff83763a32df77
Joe Blurton <j.blurton@fictivefish.com>
2019-02-01 11:01:45 +0000

    Refactored MenuExtraContent for concision.

--- src/components/MenuExtraContent.js
+++ src/components/MenuExtraContent.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Button} from 'primereact/button';
 
-class MenuExtraContent extends Component {
-	
-	render() {
-		return <Button label="Logout" icon="pi pi-power-off" style={{marginLeft:4}}/>
-	}
-}
+const MenuExtraContent  = () => (
+	<Button label="Logout" icon="pi pi-power-off" style={{marginLeft:4}}/>
+)
 
 export default MenuExtraContent;
